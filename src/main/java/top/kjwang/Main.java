3545d3c7b1a5ea0e959838bd244d84c0127be5ee
kjwang <1729298243@qq.com>
2023-03-08 21:01:36 +0800

    Initial commit

---
+++ src/main/java/top/kjwang/Main.java
@@ -0,0 +1,8 @@
+package top.kjwang;/**
+* @author kjwang
+* @Date ${DATE} ${TIME}
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
